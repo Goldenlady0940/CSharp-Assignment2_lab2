1097667de3cac78aa4a4601659d864ed32a91883
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-24 21:31:08 +0300

    Multiplication Table

---
+++ CSharpAssignment2/CSharpAssignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i, j;
+
+Console.WriteLine("Multiplication Table\n");
+Console.WriteLine("Enter your table size");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+if(size == 0)
+{
+    Console.WriteLine("Please enter number greater than one: ");
+    size = Convert.ToInt32(Console.ReadLine());
+}
+else
+{
+    for (i = 1; i <= size; i++)
+    {
+        for (j = 1; j <= size; j++)
+        {
+            Console.Write(i * j + "\t");
+        }
+        Console.Write("\n");
+    }
+}
+
